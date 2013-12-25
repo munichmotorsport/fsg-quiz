a68b260e03161833217b0f2662d6dc85eafda62d
Florian Eich <eichf@cip.ifi.lmu.de>
2013-12-25 15:59:56 +0100

    Beautified a lot of stuff. HTML/CSS can be really tedious... Bootstrap helps, but doesn't take the pain fully away.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,7 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+$('.dropdown-menu').find('form').click(function (e) {
+    e.stopPropagation();
+  });
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 FsgQuiz::Application.routes.draw do
   devise_for :views
   devise_for :users
+  
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
