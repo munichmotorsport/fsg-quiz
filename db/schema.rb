b73315fe917721d97069faaf3217020763ebfe77
Florian Eich <eichf@cip.ifi.lmu.de>
2013-12-27 03:24:50 +0100

    Added questions and answers; scaffold for the former, only model for the latter. Also added nested_forms gem to ease creation of answers. Beautified a lot more stuff, added link for question creation (only accessible for admins), generally overhauled the entire thing a bit. Not everything works right now, but this is a dev branch and I'm f...ing tired.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131224221225) do
+ActiveRecord::Schema.define(version: 20131226094710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: true do |t|
+    t.text     "content"
+    t.boolean  "correct"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
+
+  create_table "questions", force: true do |t|
+    t.text     "content"
+    t.integer  "year"
+    t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",    null: false
