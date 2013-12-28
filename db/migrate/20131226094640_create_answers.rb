096d54e61dfc5297a0722855a3113f767b29370c
Florian Eich <eichf@cip.ifi.lmu.de>
2013-12-28 21:22:51 +0100

    Sooooo many changes. Beautification of tons of stuff has happened, we now have a bit more functionality for admins and a few more forms. Also the data model for questions and answers is complete and questions can be added with answers and answers can be marked as being correct. Deletion is also possible. I'm sure I'm forgetting quite a few things right now. Next steps are: Implement a functionality to become an admin via a global password and then, of course, the quiz functionality itself.

--- db/migrate/20131226094640_create_answers.rb
+++ db/migrate/20131226094640_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :content
-      t.boolean :correct, default: => false
+      t.boolean :correct, :default => false
       t.belongs_to :question, index: true
 
       t.timestamps
